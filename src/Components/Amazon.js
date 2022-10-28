9fe5fc0f05a435cc5aa98ec5b98b98d973718d27
Benigne <ikiben23@gmail.com>
2022-10-28 10:37:38 +0200

    initial

---
+++ src/Components/Amazon.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import list from "../data";
+import Card from "./Card";
+import "../Styles/Amazon.css";
+
+const Amazon = ({ handleClick }) => {
+  return (
+    <section>
+      {list.map((item) => (
+        <Card key={item.id} item={item} handleClick={handleClick} />
+      ))}
+    </section>
+  );
+};
+
+export default Amazon;
+
